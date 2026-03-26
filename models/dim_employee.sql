4178cd6ea651b4c48f8d3ffd006fb3c11fdf235e
Malek Alsaleh <mealsale@syr.edu>
2026-03-26 21:34:11 +0000

    ff

--- models/dim_employee.sql
+++ models/dim_employee.sql
@@ -2,9 +2,22 @@ with stg_employees as (
 
     select * from {{ source('northwind', 'employees') }}
 
+),
+
+stg_supervisors as (
+
+    select * from {{ source('northwind', 'employees') }}
+
 )
 
 select
-    {{ dbt_utils.generate_surrogate_key(['stg_employees.employeeid']) }} as employeekey,
-    stg_employees.*
-from stg_employees
+    {{ dbt_utils.generate_surrogate_key(['e.employeeid']) }} as employeekey,
+    e.employeeid,
+    concat(e.lastname, ', ', e.firstname) as employeenamelastfirst,
+    concat(e.firstname, ' ', e.lastname) as employeenamefirstlast,
+    e.title as employeetitle,
+    concat(s.lastname, ', ', s.firstname) as supervisornamelastfirst,
+    concat(s.firstname, ' ', s.lastname) as supervisornamefirstlast
+from stg_employees e
+left join stg_supervisors s
+    on e.reportsto = s.employeeid
